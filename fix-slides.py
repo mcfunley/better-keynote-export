625cd3569f4a319ed00fc25f743320bd2a2fff82
Dan McKinley <mcfunley@gmail.com>
2017-05-07 08:46:28 -0700

    html generation

--- fix-slides.py
+++ fix-slides.py
@@ -3,7 +3,7 @@ import appscript
 from argparse import ArgumentParser
 from contextlib import closing
 from glob import glob
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 import math
 import os
 from reportlab.lib.colors import HexColor
@@ -14,6 +14,7 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
 from reportlab.platypus import Paragraph, Table, TableStyle
+import shutil
 
 
 sf = TTFont('SanFrancisco', 'resources/SanFrancisco-Regular.ttf')
@@ -21,13 +22,15 @@ pdfmetrics.registerFont(sf)
 
 
 class Options(object):
-    def __init__(self, outdir, pagesize, font_size):
+    def __init__(self, outdir, pagesize, font_size, title, twitter_username):
         self.outdir = os.path.abspath(outdir)
         self.pagesize = pagesize
         self.font_size = font_size
         self.leading = 1.2 * font_size
         self.notepadding = 10
         self.font = 'SanFrancisco'
+        self.title = title
+        self.twitter_username = twitter_username
 
     @property
     def slidesdir(self):
@@ -114,13 +117,21 @@ def generate_html(opts, notes):
     def imgpath(s):
         return s.replace(opts.outdir + '/', '')
 
-    t = Template(open('resources/site.jinja', 'r').read())
+    e = Environment(loader=FileSystemLoader('resources'))
+    t = e.get_template('site.jinja')
+
     s = t.render(slides=[
         { 'image': imgpath(s), 'note': n }
         for s, n in slides_and_notes(opts, notes)
-    ])
+    ], title=opts.title, twitter_username=opts.twitter_username)
+
     outfile = os.path.join(opts.outdir, 'out.html')
     open(outfile, 'w').write(s)
+
+    shutil.copyfile(
+        'resources/presentation.css',
+        os.path.join(opts.outdir, 'presentation.css')
+    )
 
 
 def main():
@@ -133,10 +144,14 @@ def main():
                     default='1920x1080')
     ap.add_argument('-f', '--font-size', help='Font size for notes',
                     type=int, dest='font_size', default=36)
+    ap.add_argument('-t', '--title', help='Title of the presentation')
+    ap.add_argument('-u', '--twitter-username', help='Twitter for author',
+                    dest='twitter_username')
 
     args = ap.parse_args()
     pagesize = tuple([int(s) for s in args.pagesize.split('x')])
-    opts = Options(args.outdir, pagesize, args.font_size)
+    opts = Options(args.outdir, pagesize, args.font_size, args.title,
+                   args.twitter_username)
 
     print('Processing', args.keynote)
     make_dirs(opts)
