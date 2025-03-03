1e8201e22fea39df00f56360f86a64feffe6d80f
chrisfinazzo <chris.finazzo@gmail.com>
2025-03-02 19:53:08 -0500

    Revert

--- keynote_export/export.py
+++ keynote_export/export.py
@@ -49,6 +49,87 @@ class Options(object):
         return w - self.notepadding * 2
 
 
+def slides_and_notes(opts, notes):
+    return zip(sorted(glob("%s/*jpeg" % opts.slidesdir)), notes)
+
+
+def make_dirs(opts):
+    for d in (
+        opts.outdir,
+        opts.slidesdir,
+    ):
+        if not os.path.isdir(d):
+            os.mkdir(d)
+
+
+def generate_pdf(opts, notes):
+    outfile = os.path.join(opts.outdir, "out.pdf")
+
+    tallest_note = -1
+    for n in notes:
+        lines = simpleSplit(n, opts.font, opts.font_size, opts.note_width)
+        tallest_note = max(tallest_note, len(lines))
+
+    note_height = ((tallest_note + 1) * opts.leading) + (opts.notepadding * 2)
+
+    s = ParagraphStyle("note")
+    s.fontName = opts.font
+    s.textColor = "black"
+    s.alignment = TA_LEFT
+    s.fontSize = opts.font_size
+    s.leading = opts.leading
+
+    img_w, img_h = opts.pagesize
+    pagesize = (img_w, img_h + note_height)
+
+    c = canvas.Canvas(outfile, pagesize=pagesize)
+    c.setStrokeColorRGB(0, 0, 0)
+
+    for slide, note in slides_and_notes(opts, notes):
+        c.setFillColor(HexColor("#ffffff"))
+        c.rect(0, 0, img_w, img_h + note_height, fill=1)
+
+        c.drawImage(
+            slide, 0, note_height, img_w, img_h, preserveAspectRatio=True
+        )
+        c.line(0, note_height, img_w, note_height)
+
+        if note:
+            p = Paragraph(note.replace("\n", "<br/>"), s)
+            p.wrapOn(c, opts.note_width, note_height)
+            p.breakLines(opts.note_width)
+            p.drawOn(c, opts.notepadding, note_height - opts.notepadding)
+        c.showPage()
+    c.save()
+
+
+def export_keynote(filename, opts):
+    filename = os.path.abspath(filename)
+
+    keynote = appscript.app("Keynote")
+    outpath = appscript.mactypes.File(opts.slidesdir)
+    k = appscript.k
+    keynote_file = appscript.mactypes.File(filename)
+
+    with closing(keynote.open(keynote_file)) as doc:
+        notes = doc.slides.presenter_notes()
+        skipped = doc.slides.skipped()
+        notes = list(itertools.compress(notes, [not s for s in skipped]))
+
+        doc.export(
+            as_=k.slide_images,
+            to=outpath,
+            with_properties={
+                k.export_style: k.IndividualSlides,
+                k.compression_factor: 0.9,
+                k.image_format: k.JPEG,
+                k.all_stages: not opts.skip_builds,
+                k.skipped_slides: False,
+            },
+        )
+
+    return notes
+
 def generate_html(opts, notes):
     def imgpath(s):
         return s.replace(opts.outdir + "/", "")
